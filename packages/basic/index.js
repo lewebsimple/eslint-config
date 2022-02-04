1347adb365baecca50bb3e242826fe9b848a8c77
Pascal Martineau <pascal@lewebsimple.ca>
2022-02-03 21:09:15 -0500

    Add eslint-plugin-import

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -6,9 +6,16 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:import/recommended",
     "plugin:prettier/recommended",
   ],
   rules: {
+    // Import
+    "import/order": "error",
+    "import/first": "error",
+    "import/no-mutable-exports": "error",
+    "import/no-unresolved": "off",
+    "import/no-absolute-path": "off",
     // Prettier
     "prettier/prettier": ["error", {
       printWidth: 120,
