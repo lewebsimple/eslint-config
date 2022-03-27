8365595ec6f7cba71c5d700be40d982ff2e0815c
Pascal Martineau <pascal@lewebsimple.ca>
2022-03-27 15:33:51 -0400

    Replace import with sort-lines

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -6,16 +6,10 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:import/recommended",
     "plugin:prettier/recommended",
   ],
   rules: {
-    // Import
-    "import/order": "error",
-    "import/first": "error",
-    "import/no-mutable-exports": "error",
-    "import/no-unresolved": "off",
-    "import/no-absolute-path": "off",
+    "sort-imports": ["error", { "allowSeparatedGroups": true }],
     // Prettier
     "prettier/prettier": ["error", {
       printWidth: 120,
